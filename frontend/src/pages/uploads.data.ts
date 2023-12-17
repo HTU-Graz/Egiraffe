5a4bd3116e17d526ae860e5726fd6aa1ccd4db95
Tanja <git@tanja.pw>
2023-12-17 19:48:17 +0100

    Frontend: upload listing works for href navigation

--- frontend/src/pages/uploads.data.ts
+++ frontend/src/pages/uploads.data.ts
@@ -1,25 +1,9 @@
 import { RouteDataFunc } from "@solidjs/router";
 import { Resource, createResource } from "solid-js";
-import { Upload } from "../api/uploads";
+import { Upload, getUploads } from "../api/uploads";
 
 const UploadsData: RouteDataFunc<unknown, Resource<Upload[]>> = (args) => {
-  // TODO: use API
-  // const [data] = createResource(() => getUploads(args.params.id));
-  const [data] = createResource<Upload[]>(async () => {
-    const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-    await sleep(500);
-    return Array.from({ length: 20 }, (_, i) => ({
-      id: `${crypto.randomUUID()}`,
-      name: `Upload ${i}`,
-      description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam dignissimos voluptatem consequuntur fuga placeat, atque mollitia autem iusto voluptatum nulla molestiae a architecto, sint alias ipsam id voluptates! Ipsam fugit voluptas molestiae suscipit impedit sit assumenda id voluptates natus sequi atque aliquid inventore tempora repudiandae labore quo, nam mollitia sapiente.`,
-      price: i,
-      uploader: `Uploader ${i}`,
-      upload_date: new Date(Date.now() - i * 100_000_000).toISOString(),
-      last_modified_date: new Date(Date.now() - i * 100_000_000).toISOString(),
-      belongs_to: `${i % 3}`,
-      held_by: `${i % 3}`,
-    }));
-  });
+  const [data] = createResource<Upload[]>(async () => await getUploads(args.params.id));
   return data;
 };
 
