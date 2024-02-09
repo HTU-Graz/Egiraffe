856851291ece8e110c6097c7b048f01af4d3d037
Tanja <git@tanja.pw>
2024-02-09 23:43:16 +0100

    Some more frontend copy

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,11 +1,30 @@
 import { Link } from "@solidjs/router";
+import { getUniversities } from "../api/universities";
+import { For, createResource } from "solid-js";
 
 export default function Home() {
+  const [unis] = createResource(getUniversities);
+
   return (
     <section>
-      <h1 class="text-2xl font-bold">Home</h1>
-      <p class="mt-4">This is the home page.</p>
-      <Link href="/courses">Kurse</Link>
+      <h1 class="text-2xl font-bold">Willkommen bei der Egiraffe</h1>
+      <br />
+      <Link class="btn btn-outline" href="/courses">Kurse</Link>
+      <select class="btn btn-outline ml-2">
+        <option>Alle Unis</option>
+        <For each={unis()}>
+          {(uni) => (
+            <option value={uni.id}>{uni.full_name}</option>
+          )}
+        </For>
+      </select>
+      <br />
+      <h2 class="text-xl font-bold mt-4 mb-1">So geht's</h2>
+      <p>
+        Suche <em class="italic">oben in der Suchleiste</em>
+        nach Kursen, Hausübungen, und Klausuren
+        über eine vielzahl an Jahrgängen und Universitäten.
+      </p>
     </section>
   );
 }
