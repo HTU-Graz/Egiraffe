7048fb6274aaa514a15b5157f36c9647a5bcbd46
Tanja <git@tanja.pw>
2024-07-31 00:06:12 +0200

    Refactor schema to use inline FK constraints

--- backend/migrations/0001_initial.sql
+++ backend/migrations/0001_initial.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS "user" (
     id uuid PRIMARY KEY,
     first_names character varying(200),
     last_name character varying(200),
-    primary_email uuid NOT NULL,
+    primary_email uuid NOT NULL REFERENCES email (id),
     password_hash character varying(250) NOT NULL,
     totp_secret character varying(50),
     nick character varying(100),
@@ -26,37 +26,35 @@ CREATE TABLE IF NOT EXISTS "user" (
 CREATE TABLE IF NOT EXISTS email (
     id uuid PRIMARY KEY,
     address character varying(500),
-    belongs_to_user uuid NOT NULL,
-    of_university uuid,
-    STATUS character(20) NOT NULL
+    belongs_to_user uuid NOT NULL REFERENCES "user" (id),
+    of_university uuid REFERENCES university (id),
+    "status" character(20) NOT NULL REFERENCES email_status ("status")
 );
 
-CREATE TABLE IF NOT EXISTS email_status (
-    STATUS character(20) NOT NULL PRIMARY KEY
-);
+CREATE TABLE IF NOT EXISTS email_status ("status" character(20) NOT NULL PRIMARY KEY);
 
 CREATE TABLE IF NOT EXISTS upload (
     id uuid PRIMARY KEY,
     upload_name character varying(200) NOT NULL,
     description text NOT NULL,
     price smallint NOT NULL,
-    uploader uuid NOT NULL,
+    uploader uuid NOT NULL REFERENCES "user" (id),
     upload_date timestamp without time zone NOT NULL,
     last_modified_date timestamp without time zone NOT NULL,
-    belongs_to uuid NOT NULL,
-    held_by uuid
+    belongs_to uuid NOT NULL REFERENCES course (id),
+    held_by uuid REFERENCES prof (id),
 );
 
 CREATE TABLE IF NOT EXISTS course (
     id uuid NOT NULL PRIMARY KEY,
-    held_at uuid NOT NULL,
+    held_at uuid NOT NULL REFERENCES university (id),
     course_name character varying(200) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS purchase (
     -- This table uses a composite primary key
-    user_id uuid,
-    upload_id uuid,
+    user_id uuid REFERENCES "user" (id),
+    upload_id uuid REFERENCES upload (id),
     ecs_spent smallint NOT NULL,
     purchase_date timestamp without time zone NOT NULL,
     rating smallint,
@@ -65,16 +63,16 @@ CREATE TABLE IF NOT EXISTS purchase (
 
 CREATE TABLE IF NOT EXISTS system_ec_transaction (
     -- This table uses a composite primary key
-    affected_user uuid,
+    affected_user uuid REFERENCES "user" (id),
     transaction_date timestamp without time zone NOT NULL,
     delta_ec bigint NOT NULL,
     reason character varying(1000),
     PRIMARY KEY (affected_user, transaction_date)
 );
 
-CREATE TABLE IF NOT EXISTS SESSION (
+CREATE TABLE IF NOT EXISTS "session" (
     id uuid PRIMARY KEY,
-    of_user uuid,
+    of_user uuid REFERENCES "user" (id),
     token character(43) NOT NULL,
     initial_user_agent character varying(275),
     latest_user_agent character varying(275),
@@ -87,118 +85,15 @@ CREATE TABLE IF NOT EXISTS prof (
     prof_name character varying(500) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS FILE (
+CREATE TABLE IF NOT EXISTS "file" (
     id uuid PRIMARY KEY,
     name character varying(255) NOT NULL,
     mime_type character varying(200) NOT NULL,
     size bigint NOT NULL,
     revision_at timestamp without time zone NOT NULL,
-    upload_id uuid NOT NULL,
+    upload_id uuid NOT NULL REFERENCES upload (id),
     approval_uploader boolean NOT NULL,
     approval_mod boolean NOT NULL
 );
-
-ALTER TABLE IF EXISTS public."user"
-    ADD FOREIGN KEY (primary_email)
-    REFERENCES public.email (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
-
-
-ALTER TABLE IF EXISTS public.email
-    ADD CONSTRAINT belongs_to_user_fk FOREIGN KEY (belongs_to_user)
-    REFERENCES public."user" (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
-
-
-ALTER TABLE IF EXISTS public.email
-    ADD CONSTRAINT of_university_fk FOREIGN KEY (of_university)
-    REFERENCES public.university (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
-
-
-ALTER TABLE IF EXISTS public.email
-    ADD CONSTRAINT email_status_fk FOREIGN KEY (status)
-    REFERENCES public.email_status (status) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
-
-
-ALTER TABLE IF EXISTS public.upload
-    ADD FOREIGN KEY (uploader)
-    REFERENCES public."user" (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
-
-
-ALTER TABLE IF EXISTS public.upload
-    ADD FOREIGN KEY (belongs_to)
-    REFERENCES public.course (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
-
-
-ALTER TABLE IF EXISTS public.upload
-    ADD FOREIGN KEY (held_by)
-    REFERENCES public.prof (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
-
-
-ALTER TABLE IF EXISTS public.course
-    ADD FOREIGN KEY (held_at)
-    REFERENCES public.university (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
-
-
-ALTER TABLE IF EXISTS public.purchase
-    ADD FOREIGN KEY (user_id)
-    REFERENCES public."user" (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
-
-
-ALTER TABLE IF EXISTS public.purchase
-    ADD FOREIGN KEY (upload_id)
-    REFERENCES public.upload (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
-
-
-ALTER TABLE IF EXISTS public.system_ec_transaction
-    ADD FOREIGN KEY (affected_user)
-    REFERENCES public."user" (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
-
-
-ALTER TABLE IF EXISTS public.session
-    ADD FOREIGN KEY (of_user)
-    REFERENCES public."user" (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
-
-
-ALTER TABLE IF EXISTS public.file
-    ADD FOREIGN KEY (upload_id)
-    REFERENCES public.upload (id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID;
 
 END;
