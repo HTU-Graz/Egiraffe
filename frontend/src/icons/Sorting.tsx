4b66daf7a546677a52e747382fffddd7c3337716
Tanja <git@tanja.pw>
2024-10-27 15:16:55 +0100

    Frontend: refactor icons

---
+++ frontend/src/icons/Sorting.tsx
@@ -0,0 +1,40 @@
+export const AscendingIcon = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="28"
+        height="28"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M4 6l7 0" />
+        <path d="M4 12l7 0" />
+        <path d="M4 18l9 0" />
+        <path d="M15 9l3 -3l3 3" />
+        <path d="M18 6l0 12" />
+    </svg>
+);
+
+export const DescendingIcon = () => (
+    <svg
+        width="28"
+        height="28"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M4 6l9 0" />
+        <path d="M4 12l7 0" />
+        <path d="M4 18l7 0" />
+        <path d="M15 15l3 3l3 -3" />
+        <path d="M18 6l0 12" />
+    </svg>
+);
