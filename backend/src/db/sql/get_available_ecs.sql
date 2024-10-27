98d8fa2d6756baba1e0153c4b33ddb3198898e87
Tanja <git@tanja.pw>
2024-10-27 23:07:41 +0100

    Backend: #3 !5 format get_available_ecs.sql

--- backend/src/db/sql/get_available_ecs.sql
+++ backend/src/db/sql/get_available_ecs.sql
@@ -8,28 +8,42 @@
 WITH cte AS (
     -- ECS earned from uploads
     ecs_earned AS (
-        SELECT SUM(pu.ecs_spent * 0.8)
-        FROM upload AS up
+        SELECT
+            SUM(pu.ecs_spent * 0.8)
+        FROM
+            upload AS up
             INNER JOIN purchase AS pu ON pu.upload_id = up.id
             INNER JOIN "user" AS us ON us.id = up.uploader
-        WHERE up.uploader = $1
-        AND pu.user_id <> up.uploader -- exclude self-purchases
-        GROUP BY up.uploader
+        WHERE
+            up.uploader = $ 1
+            AND pu.user_id <> up.uploader -- exclude self-purchases
+        GROUP BY
+            up.uploader
     ),
     -- ECS spent on purchases
     ecs_spent AS (
-        SELECT SUM(pu.ecs_spent)
-        FROM purchase AS pu
-        WHERE pu.user_id = $1
-        GROUP BY pu.user_id
+        SELECT
+            SUM(pu.ecs_spent)
+        FROM
+            purchase AS pu
+        WHERE
+            pu.user_id = $ 1
+        GROUP BY
+            pu.user_id
     ),
     -- ECS given/taken by the system
     ecs_system AS (
-        SELECT SUM(systrans.amount)
-        FROM system_ec_transaction AS systrans
-        WHERE systrans.affected_user = $1
-        GROUP BY systrans.affected_user
+        SELECT
+            SUM(systrans.amount)
+        FROM
+            system_ec_transaction AS systrans
+        WHERE
+            systrans.affected_user = $ 1
+        GROUP BY
+            systrans.affected_user
     )
 )
-SELECT ecs_earned + ecs_system - ecs_spent AS ecs_available
-FROM cte
+SELECT
+    ecs_earned + ecs_system - ecs_spent AS ecs_available
+FROM
+    cte
