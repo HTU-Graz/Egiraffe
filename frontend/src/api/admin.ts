dc294c3dd21e391e5b56ea7b427d6f0de506c00c
Tanja <git@tanja.pw>
2024-11-13 23:05:22 +0100

    Frontend: better Admin User-List debug page

--- frontend/src/api/admin.ts
+++ frontend/src/api/admin.ts
@@ -5,9 +5,35 @@ export type GetAllUsersResponse =
   | ErrorResponse
   | { success: true; users: RedactedUser[] };
 
+export type UserBalanceResponse =
+  | ErrorResponse
+  | { success: true; balance: number };
+
+export interface CreateSystemTransactionRequest {
+  user_id: string,
+  delta_ec: number,
+  reason?: string,
+};
+
+export type CreateSystemTransactionResponse =
+  | ErrorResponse
+  | { success: true; };
 
 export async function getAllUsers(): Promise<RedactedUser[]> {
   const response = await put<GetAllUsersResponse>("/api/v1/admin/users/get-users");
   if (!response.success) throw new Error(response.message);
   return response.users;
+}
+
+export async function getUserBalance(user_id: string): Promise<number> {
+  const response = await put<UserBalanceResponse>("/api/v1/admin/ecs/get-user-balance", { user_id });
+  if (!response.success) throw new Error(response.message);
+  return response.balance;
+}
+
+export async function createSystemTransaction(
+  request: CreateSystemTransactionRequest
+): Promise<void> {
+  const response = await put<GetAllUsersResponse>("/api/v1/admin/ecs/create-system-transaction", request);
+  if (!response.success) throw new Error(response.message);
 }
--- frontend/src/pages/admin/users.tsx
+++ frontend/src/pages/admin/users.tsx
@@ -2,6 +2,7 @@ import { useRouteData } from "@solidjs/router";
 import { createResource, createSignal, For, Match, Show, Switch } from "solid-js";
 import { AscendingIcon, DescendingIcon } from "../../icons/Sorting";
 import { getAllUsers } from "../../api/admin";
+import { RedactedUser } from "../../api/users";
 
 /**
  * @file This page displays and manages all users in the system. It is only accessible to administrators.
@@ -23,34 +24,58 @@ export default function Users() {
                 </Match>
                 <Match when={users()}>
                     <For each={users()!!}>
-                        {(user) => (
-                            <div class="card shadow-md">
-                                <div class="card-body">
-                                    <div class="grid grid-cols-2">
-                                        <span>User ID</span>
-                                        <span>{user.id}</span>
-                                        <span>First Names</span>
-                                        <span>{user.first_names}</span>
-                                        <span>Last Name</span>
-                                        <span>{user.last_name}</span>
-                                        <span>2FA Enabled</span>
-                                        <span>{user.totp_enabled ? "Yes" : "No"}</span>
-                                        <span>User Role</span>
-                                        <span>{user.user_role}</span>
-
-                                        <span>Add/remove ECs</span>
-                                        <div>
-                                            <input></input>
-                                            <button class="btn">Todo</button>
-                                        </div>
-                                    </div>
-
-                                </div>
-                            </div>
-                        )}
+                        {user => <UserCard user={user} />}
                     </For>
                 </Match>
             </Switch>
         </>
+    );
+}
+
+function UserCard({ user }: { user: RedactedUser }) {
+    const [ecInput, setEcInput] = createSignal("");
+    const [reasonInput, setReasonInput] = createSignal("");
+
+    const ecInputId = `ecInput-${user.id}`;
+    const reasonInputId = `reasonInput-${user.id}`;
+
+    return (
+        <div class="card shadow-md">
+            <p>
+                {ecInput()}
+            </p>
+            <div class="card-body">
+                <div class="grid grid-cols-[auto,auto]">
+                    <span>User ID</span>
+                    <span>{user.id}</span>
+                    <span>First Names</span>
+                    <span>{user.first_names}</span>
+                    <span>Last Name</span>
+                    <span>{user.last_name}</span>
+                    <span>2FA Enabled</span>
+                    <span>{user.totp_enabled ? "Yes" : "No"}</span>
+                    <span>User Role</span>
+                    <span>{user.user_role}</span>
+
+                    <span>Add/remove ECs</span>
+                    <div class="flex flex-row gap-2 items-baseline">
+                        <div class="form-control">
+                            <label class="label" for={ecInputId}>
+                                <span class="label-text">EC Input</span>
+                            </label>
+                            <input class="input input-bordered" type="number" id={ecInputId} value={ecInput()} onInput={(e) => setEcInput(e.currentTarget.value)} />
+                        </div>
+                        <div class="form-control">
+                            <label class="label" for={reasonInputId}>
+                                <span class="label-text">Reason</span>
+                            </label>
+                            <input class="input input-bordered" type="text" id={reasonInputId} value={reasonInput()} onInput={(e) => setReasonInput(e.currentTarget.value)} />
+                        </div>
+                        <button class="btn">Create system transaction</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     );
 }
