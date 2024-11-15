a6f74f8904abacb18d33197072b9e28e9951330e
Tanja <git@tanja.pw>
2024-11-15 01:41:17 +0100

    Backend & db: fix `get_available_ecs.sql`

--- backend/src/db/sql/get_available_ecs.sql
+++ backend/src/db/sql/get_available_ecs.sql
@@ -1,50 +1,34 @@
--- Calculates the amount of ECS the user has available to spend
---
--- Takes into account:
---
--- - ECS the user has earned (when a purchased upload is owned by the user)
--- - When a user has spent ECS on a purchase
--- - When the system has given/taken ECS from the user (see table `system_ec_transaction`)
--- ECS earned from uploads
-WITH -- 
+WITH -- ECS earned from uploads
 ecs_earned_tbl AS (
     SELECT
-        SUM(pu.ecs_spent * 0.8) AS ecs_earned
+        COALESCE(SUM(pu.ecs_spent * 0.8), 0) AS ecs_earned
     FROM
         upload AS up
         INNER JOIN purchase AS pu ON pu.upload_id = up.id
-        INNER JOIN "user" AS us ON us.id = up.uploader
     WHERE
         up.uploader = $1
         AND pu.user_id <> up.uploader -- exclude self-purchases
-    GROUP BY
-        up.uploader
 ),
 -- ECS spent on purchases
 ecs_spent_tbl AS (
     SELECT
-        SUM(pu.ecs_spent) AS ecs_spent
+        COALESCE(SUM(pu.ecs_spent), 0) AS ecs_spent
     FROM
         purchase AS pu
     WHERE
         pu.user_id = $1
-    GROUP BY
-        pu.user_id
 ),
 -- ECS given/taken by the system
 ecs_system_tbl AS (
     SELECT
-        SUM(systrans.delta_ec) AS ecs_system
+        COALESCE(SUM(systrans.delta_ec), 0) AS ecs_system
     FROM
         system_ec_transaction AS systrans
     WHERE
         systrans.affected_user = $1
-    GROUP BY
-        systrans.affected_user
 )
 SELECT
-    -- cast this to anything that's not numeric, but has the largest possible length
-    FOR reference: ecs_earned + ecs_system - ecs_spent AS ecs_available
+    ecs_earned + ecs_system - ecs_spent AS ecs_available
 FROM
     ecs_earned_tbl
     CROSS JOIN ecs_spent_tbl
